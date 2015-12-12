dca32691c5159a9dd8009b028bfc6ed6437e0543
Jean-Francois Brouillet <verec@mac.com>
2015-12-12 11:45:36 +0000

    First Ever Commit (Local)

--- DynamicTableViewDataSource/AppDelegate.swift
+++ DynamicTableViewDataSource/AppDelegate.swift
@@ -2,45 +2,47 @@
 //  AppDelegate.swift
 //  DynamicTableViewDataSource
 //
-//  Created by verec on 12/12/2015.
+//  Created by verec on 28/11/2015.
 //  Copyright © 2015 Cantabilabs Ltd. All rights reserved.
 //
 
 import UIKit
+
+func scope(@noescape lambda: ()->()) {
+    lambda()
+}
+
+class RAII {
+    init(appDelegate: AppDelegate, @noescape lamda:()->()) {
+        /// This creates a default blank view that is fullscreen and supports
+        /// all four orientations
+        Top.defaultWindow.rootViewController = Top.defaultController
+        Top.defaultController.view = Top.mainView
+        appDelegate.window = Top.defaultWindow
+
+        lamda()
+    }
+
+    deinit {
+        Top.defaultWindow.makeKeyAndVisible()
+    }
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(application: UIApplication
+    ,   didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        scope {
+            let _ = RAII(appDelegate: self) {
+                Top.mainView.backgroundColor = UIColor.redColor()
+                Top.mainView.addSubview(Views.tableView)
+            }
+        }
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
